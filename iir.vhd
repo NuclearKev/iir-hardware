8bbc9441c979bdf7395f475c0525e3604519e8f7
Kevin Bloom <kdb5@pct.edu>
2017-01-13 17:20:32 -0500

    Attempting to get things working

--- iir.vhd
+++ iir.vhd
@@ -24,10 +24,11 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use IEEE.STD_LOGIC_SIGNED.ALL;
 
 -- As of now, the coefficients will be internally set contstants
 entity iir is
-  Port ( Clk      : in STD_LOGIC;       --probably should be a fast non-fs clock
+  Port ( in_clk   : in STD_LOGIC;       --probably should be a fast non-fs clock
          data_in  : in STD_LOGIC_VECTOR  (11 downto 0);
          data_out : out STD_LOGIC_VECTOR (11 downto 0));
 end iir;
@@ -37,7 +38,7 @@ architecture Behavioral of iir is
   signal fir_out : STD_LOGIC_VECTOR(11 downto 0);
 
   -- Defining of the coefficient arrays
-  type a_int is array (4 downto 0) of integer;
+  type a_int is array (4 downto 0) of STD_LOGIC_VECTOR(11 downto 0);
   -- type a_elem is array (8 downto 0) of a;
   -- type b is array (14 downto 0) of integer;
   -- type b_elem is array (9 downto 0) of b;
@@ -45,23 +46,23 @@ architecture Behavioral of iir is
   -- signal b_coeff : b_elem;
 
   signal a : a_int := (
-    0 => 1,
-    1 => 2,
-    2 => 3,
-    3 => 4,
-    4 => 5,
-    others => 0);
+    0 => x"1999",
+    1 => x"1999",
+    2 => x"1999",
+    3 => x"1999",
+    4 => x"1999",
+    others => x"1999");
 
   -- Data array
-  type data_array is array (4 downto 0) of integer;
+  type data_array is array (4 downto 0) of STD_LOGIC_VECTOR(11 downto 0);
   -- type data_elem is array (9 downto 0) of data_array;
   signal data : data_array := (
-    0 => 5,
-    1 => 4,
-    2 => 3,
-    3 => 2,
-    4 => 1,
-    others => 0);
+    0 => x"5",
+    1 => x"4",
+    2 => x"3",
+    3 => x"2",
+    4 => x"1",
+    others => x"0");
 
 begin
 
@@ -75,21 +76,26 @@ begin
   -- data <= ( x"0009", x"0008", x"0007", x"0006", x"0005", x"0004", x"0003",
   --          x"0002", x"0001", x"0000" );
 
-  fir: process(Clk,data)
+  fir: process(in_clk,data)
+    
+  variable i : integer := 0;
+  variable big_output : STD_LOGIC_VECTOR(23 downto 0);
+  
   begin
-
+    
     -- variable buf : integer := 0;        -- Declare loop variable here
-    if(rising_edge(Clk)) then
+    if(rising_edge(in_clk)) then
 
-      -- Shift the data array and feed in the new data
-      variable i : integer := 0;
-      for i in 0 to 4 loop
+      -- Shift the data array and feed in the new data      
+      for i in 0 to 3 loop
         data(i+1) <= data(i);
       end loop;
       data(0) <= data_in;
 
-      fir_out <= data(0)*a(0) + data(1)*a(1) + data(2)*a(2) + data(3)*a(3) +
+      big_output := data(0)*a(0) + data(1)*a(1) + data(2)*a(2) + data(3)*a(3) +
                  data(4)*a(4);
+      
+      fir_out <= big_output(11 downto 0);                
 
     end if;
     -- Right shift the data array here
